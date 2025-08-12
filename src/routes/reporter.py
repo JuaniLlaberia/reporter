d7c89c1906213828007899a65988380e992ac6a4
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-08-12 19:24:28 -0300

    Add reporter route

--- src/routes/reporter.py
+++ src/routes/reporter.py
@@ -1,7 +1,25 @@
 from flask import Blueprint, request, jsonify
+from src.agents.orchestrator.orchestrator import Orchestrator
 
 reporter_bp = Blueprint("reporter", __name__)
 
 @reporter_bp.route("/generate-report", methods=["POST"])
 def generate_report():
-    pass
+    """
+    """
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    request_json = request.get_json()
+    if "prompt" not in request_json:
+        return jsonify({"error": "Missing prompt"}), 400
+
+    orchestrator = Orchestrator(
+        ollama_model="gemma3:4b",
+        ollama_base_url="http://localhost:11434"
+    )
+
+    prompt = request_json["prompt"]
+    orchestrator.run(prompt)
+
+    return jsonify({"message": "Report was successfully created"}), 200
